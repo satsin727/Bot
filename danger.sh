4ae2f6d156712ffdc7e679b183605058ab9a2be2
satsin727 <satsin727@gmail.com>
2024-09-26 11:50:14 +0530

    Update danger.sh
    Corrected file path

--- danger.sh
+++ danger.sh
@@ -3,7 +3,7 @@ cat <<EOL > .devcontainer/devcontainer.json
 {
     "name": "My Codespace",
     "image": "mcr.microsoft.com/vscode/devcontainers/python:3.8",
-    "postStartCommand": "python3 /workspaces/Bot/danger.py",
+    "postStartCommand": "python3 /opt/render/project/src/danger.py",
     "customizations": {
         "vscode": {
             "settings": {
